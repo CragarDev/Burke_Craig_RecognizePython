0b375590e9355d9bd6f31c0c357a62b34961e64e
Craig Burke <cragardev@gmail.com>
2022-03-05 20:45:26 -0600

    half way done

--- recogniizePython_file.py
+++ recogniizePython_file.py
@@ -1,56 +1,56 @@
-num1 = 42  # - variable declaration - Data Types- Primitive- Numbers
-num2 = 2.3  # - variable declaration - Data Types- Primitive- Numbers
-boolean = True  # - variable declaration - Data Types- Primitive- Boolean
-string = 'Hello World'  # - variable declaration - Data Types- Primitive- Strings
-# - variable declaration - Data Types- Composite- List- initialize
+num1 = 42  # - variable declaration | - Data Types- Primitive- Numbers
+num2 = 2.3  # - variable declaration | - Data Types- Primitive- Numbers
+boolean = True  # - variable declaration | - Data Types- Primitive- Boolean
+string = 'Hello World'  # - variable declaration | - Data Types- Primitive- Strings
+# - variable declaration | - Data Types- Composite - List - initialize
 pizza_toppings = ['Pepperoni', 'Sausage', 'Jalepenos', 'Cheese', 'Olives']
 person = {'name': 'John', 'location': 'Salt Lake',
-          'age': 37, 'is_balding': False}  # - variable declaration - Data Types - Primitive - Boolean - Numbers - Composite - List - initialize
-fruit = ('blueberry', 'strawberry', 'banana')  # - variable declaration
-print(type(fruit))  # - log statement - type check
-print(pizza_toppings[1])  # - log statement
-pizza_toppings.append('Mushrooms')
-print(person['name'])  # - log statement
-person['name'] = 'George'
-person['eye_color'] = 'blue'
-print(fruit[2])
+          'age': 37, 'is_balding': False}  # - variable declaration | - Data Types - Composite - Dictionary - initialize
+fruit = ('blueberry', 'strawberry', 'banana')  # - variable declaration | - Data Types - Composite - Tuples - initialize
+print(type(fruit))  # - log statement | - type check | - Data Types - Composite - Tuples - access value
+print(pizza_toppings[1])  # - log statement | - Data Types - Composite - List - access value
+pizza_toppings.append('Mushrooms') # - Data Types - Composite - List - change value - add value
+print(person['name'])  # - log statement | - Data Types - Composite - Dictionary - access value
+person['name'] = 'George' # - Data Types - Composite - Dictionary - access value - change value
+person['eye_color'] = 'blue' # - Data Types - Composite - Dictionary - access value - add value
+print(fruit[2]) # - log statement | - Data Types - Composite - Tuples - access value
 
-if num1 > 45:
+if num1 > 45: # - conditional - if
     print("It's greater")  # - log statement
-else:
+else: # - conditional - else
     print("It's lower")  # - log statement
 
-if len(string) < 5:  # - length check
+if len(string) < 5:  # - length check | - conditional - if
     print("It's a short word!")  # - log statement
-elif len(string) > 15:  # - length check
+elif len(string) > 15:  # - length check | - conditional - else if
     print("It's a long word!")  # - log statement
-else:
+else: # - conditional - else
     print("Just right!")  # - log statement
 
-for x in range(5):
-    print(x)  # - log statement
-for x in range(2, 5):
-    print(x)  # - log statement
-for x in range(2, 10, 3):
-    print(x)  # - log statement
+for x in range(5):  # - for loop - start - increment - stop
+    print(x)  # - log statement | - for loop - stop
+for x in range(2, 5):  # - for loop - start - increment - stop
+    print(x)  # - log statement | - for loop - stop
+for x in range(2, 10, 3):  # - for loop - start - increment - stop
+    print(x)  # - log statement | - for loop - stop
 x = 0
 while(x < 5):
     print(x)  # - log statement
     x += 1
 
-pizza_toppings.pop()
-pizza_toppings.pop(1)
+pizza_toppings.pop()  # - Data Types - Composite - List - change value - delete value
+pizza_toppings.pop(1) # - Data Types - Composite - List - change value - delete value
 
 print(person)  # - log statement
-person.pop('eye_color')
+person.pop('eye_color') # - Data Types - Composite - Dictionary - access value - delete value
 print(person)  # - log statement
 
-for topping in pizza_toppings:
-    if topping == 'Pepperoni':
-        continue
+for topping in pizza_toppings: # - Data Types - Composite - List - access value | - for loop - start - sequence
+    if topping == 'Pepperoni': # - conditional - if
+        continue # - for loop - continue
     print('After 1st if statement')  # - log statement
-    if topping == 'Olives':
-        break
+    if topping == 'Olives': # - conditional - if
+        break # - for loop - break
 
 
 def print_hello_ten_times():
@@ -74,19 +74,27 @@ def print_hello_x_or_ten_times(x=10):
         print('Hello')  # - log statement
 
 
-print_hello_x_or_ten_times()  # - log statement
-print_hello_x_or_ten_times(4)  # - log statement
+print_hello_x_or_ten_times()
+print_hello_x_or_ten_times(4)
 
 
 """
 Bonus section - comment - multiline
 """
 
-# print(num3) # - log statement - comment - single line
-# num3 = 72 #- comment - single line
-# fruit[0] = 'cranberry' #- comment - single line
-# print(person['favorite_team']) #- comment - single line - log statement
-# print(pizza_toppings[7]) #- comment - single line - log statement
-#   print(boolean) #- comment - single line - log statement
-# fruit.append('raspberry') #- comment - single line
-# fruit.pop(1) #- comment - single line
+# print(num3) # - log statement - comment - single line | - NameError: name <variable name> is not defined
+# num3 = 72 # - comment - single line
+# fruit[0] = 'cranberry' #- comment - single line | - TypeError: 'tuple' object does not support item assignment
+# print(person['favorite_team']) #- comment - single line - log statement | - KeyError: 'favorite_team'
+# print(pizza_toppings[7]) #- comment - single line - log statement | - IndexError: list index out of range
+#   print(boolean) #- comment - single line - log statement | - IndentationError: unexpected indent
+# fruit.append('raspberry') #- comment - single line | - AttributeError: 'tuple' object has no attribute 'append'
+# fruit.pop(1) #- comment - single line | - AttributeError: 'tuple' object has no attribute 'pop'
+
+""" 
+cannot change, add, or delete with a Tuple
+- Tuples
+    - change value
+    - add value
+    - delete value
+"""
